3bfa6bfbdbe0771c095918e1bca5785481f5b91c
NINJA326 <xxxmastsxxx@gmail.com>
2026-07-06 21:29:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// NINJA AIRS Version20.8 cache reset service worker
-const CACHE_VERSION='ninja-airs-v20-8-reset';
+// NINJA AIRS Version20.10 cache reset service worker
+const CACHE_VERSION='ninja-airs-v20-10-reset';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request));});
